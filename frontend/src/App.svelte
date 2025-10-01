dc7ece9c61c8505f010ab387e5bc2fb4127ba08c
Febrian Amir <febrianamir100@gmail.com>
2025-10-01 11:32:09 +0800

    Revert "feat: setup shadcn library"
    This reverts commit 0ecf931e3a7480882713f6015ec69d6a087539f3.

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,11 +1,9 @@
 <script>
-  import { Button } from "$lib/components/ui/button/index.js";
   console.log("capydb running");
 </script>
 
 <main>
   <h1>CapyDB</h1>
-  <Button>Click me</Button>
 </main>
 
 <style>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,7 @@
-import tailwindcss from '@tailwindcss/vite';
-import path from "path";
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), svelte()],
-  resolve: {
-    alias: {
-      $lib: path.resolve("./src/lib"),
-    },
-  },
-});
+  plugins: [svelte()]
+})
