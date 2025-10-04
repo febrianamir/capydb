f9d2ff4603cb51507260b6ae9d621f957a3764f3
Febrian Amir <febrianamir100@gmail.com>
2025-10-04 23:56:27 +0800

    fix(table): add db conn check to prevent panic

--- backend/usecase/table.go
+++ backend/usecase/table.go
@@ -4,10 +4,15 @@ import (
 	"capydb/backend/dto/request"
 	"capydb/backend/dto/response"
 	"capydb/backend/model"
+	"errors"
 	"fmt"
 )
 
 func (u *Usecase) GetTables() ([]string, error) {
+	if u.dbConn == nil {
+		return []string{}, errors.New("no db connection")
+	}
+
 	query := "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY table_name ASC"
 
 	var tableNames = make([]string, 0)
@@ -20,6 +25,10 @@ func (u *Usecase) GetTables() ([]string, error) {
 }
 
 func (u *Usecase) GetTableColumns(tableName string) ([]map[string]any, error) {
+	if u.dbConn == nil {
+		return []map[string]any{}, errors.New("no db connection")
+	}
+
 	query := fmt.Sprintf("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '%v' ORDER BY ordinal_position", tableName)
 
 	var columns = make([]map[string]any, 0)
@@ -32,6 +41,10 @@ func (u *Usecase) GetTableColumns(tableName string) ([]map[string]any, error) {
 }
 
 func (u *Usecase) GetTableRecords(req request.GetTableRecords) (response.GetTableRecords, error) {
+	if u.dbConn == nil {
+		return response.GetTableRecords{}, errors.New("no db connection")
+	}
+
 	var res response.GetTableRecords
 
 	query := fmt.Sprintf("SELECT * FROM %v", req.TableName)
