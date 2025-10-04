0cfbea0d3dda184221275e9a5c1ced1729d4dedd
Febrian Amir <febrianamir100@gmail.com>
2025-10-04 23:44:30 +0800

    feat: maximised window on startup

--- main.go
+++ main.go
@@ -20,9 +20,10 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "CapyDB",
-		Width:  1024,
-		Height: 768,
+		Title:            "CapyDB",
+		Width:            1024,
+		Height:           768,
+		WindowStartState: options.Maximised,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
