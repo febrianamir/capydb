66b984ab119b181288587e2ade1f0f60034ac6f9
Febrian Amir <febrianamir100@gmail.com>
2025-10-12 12:33:34 +0800

    refactor(table): TableRecord component

--- frontend/src/components/tables/Table.svelte
+++ frontend/src/components/tables/Table.svelte
@@ -5,10 +5,10 @@
     GetTableRecords,
     GetTables,
   } from "../../../wailsjs/go/usecase/Usecase";
-  import { ArrowDownNarrowWide, ArrowUpWideNarrow } from "@lucide/svelte";
   import TableSidebar from "./TableSidebar.svelte";
   import TableToolbar from "./TableToolbar.svelte";
   import TableFilter from "./TableFilter.svelte";
+  import TableRecord from "./TableRecord.svelte";
 
   let isShowTableList = $state(false);
   let isShowTableFilter = $state(false);
@@ -72,50 +72,6 @@
     } catch (err) {
       console.log("Failed to get table contents:", err);
     }
-  }
-
-  function getColumnTypeColorClass(type, data) {
-    if (data === null) {
-      return "table-data-cell-null";
-    }
-
-    switch (type) {
-      case "integer":
-      case "numeric":
-      case "double precision":
-        return "table-data-cell-blue";
-      case "character varying":
-      case "text":
-        return "table-data-cell-green";
-      case "timestamp with time zone":
-      case "timestamp without time zone":
-        return "table-data-cell-orange";
-      case "boolean":
-        return "table-data-cell-yellow";
-    }
-  }
-
-  function toggleColumnSort(tableColumn) {
-    // If sort_by changed, set order_by back to ASC
-    // If sort_by unchanged, toggle order_by
-    let sortBy = tableColumn.column_name;
-    let orderBy = "";
-    if (tableColumn.column_name != queryTableContents.sort_by) {
-      orderBy = "ASC";
-    } else {
-      if (queryTableContents.order_by == "ASC") {
-        orderBy = "DESC";
-      } else {
-        sortBy = "";
-        orderBy = "";
-      }
-    }
-    updateQueryTableContents({
-      table_name: queryTableContents.table_name,
-      sort_by: sortBy,
-      order_by: orderBy,
-      offset: queryTableContents.offset,
-    });
   }
 
   function toggleTableFilter(defaultColumn) {
@@ -189,58 +145,12 @@
         {queryTableContents}
         {tableColumns}
       />
-      <table class="table-data">
-        <thead class="table-data-head">
-          <tr>
-            {#each tableColumns as tableColumn}
-              <th
-                class="table-data-cell"
-                onclick={(e) => {
-                  e.preventDefault();
-                  toggleColumnSort(tableColumn);
-                }}
-              >
-                {tableColumn.column_name}
-                <div
-                  class="table-data-cell-sort {tableColumn.column_name ==
-                  queryTableContents.sort_by
-                    ? 'active'
-                    : ''}"
-                >
-                  {#if queryTableContents.order_by == "ASC"}
-                    <div class="table-data-cell-icon">
-                      <ArrowDownNarrowWide size="16" />
-                    </div>
-                  {:else}
-                    <div class="table-data-cell-icon">
-                      <ArrowUpWideNarrow size="16" />
-                    </div>
-                  {/if}
-                </div>
-              </th>
-            {/each}
-          </tr>
-        </thead>
-        <tbody class="table-data-body">
-          {#if tableRecords.length > 0}
-            {#each tableRecords as tableRecord}
-              <tr>
-                {#each tableColumns as tableColumn}
-                  <td
-                    class="table-data-cell {getColumnTypeColorClass(
-                      tableColumn.data_type,
-                      tableRecord[tableColumn.column_name],
-                    )}"
-                    >{tableRecord[tableColumn.column_name] === null
-                      ? "NULL"
-                      : tableRecord[tableColumn.column_name]}</td
-                  >
-                {/each}
-              </tr>
-            {/each}
-          {/if}
-        </tbody>
-      </table>
+      <TableRecord
+        {queryTableContents}
+        {updateQueryTableContents}
+        {tableColumns}
+        {tableRecords}
+      />
     {/if}
   </div>
 </div>
@@ -272,221 +182,5 @@
     overflow: auto;
     text-align: left;
     font-size: 14px;
-  }
-
-  /* Table Filter */
-  .table-filter {
-    display: none;
-    margin-bottom: 0.5rem;
-  }
-
-  .table-filter.active {
-    display: block;
-  }
-
-  .table-filter-item {
-    display: flex;
-    padding: 1rem 1rem 0 1rem;
-    align-items: center;
-    gap: 1rem;
-  }
-
-  .table-filter-item-inputs {
-    display: flex;
-    gap: 0.75rem;
-    align-items: center;
-    width: 100%;
-  }
-
-  .table-filter-item-check {
-    position: relative;
-  }
-
-  .table-filter-check-input {
-    appearance: none;
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    width: 1.2rem;
-    height: 1.2rem;
-    margin: 0;
-    border: 1px solid var(--color-dark-grey-2);
-    border-radius: 0.4rem;
-    cursor: pointer;
-    transition: 0.2s ease;
-  }
-
-  .table-filter-check-icon {
-    display: flex;
-    opacity: 0;
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 1.2rem;
-    height: 1.2rem;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    pointer-events: none;
-    transition: 0.1s ease;
-  }
-
-  .table-filter-check-icon.active {
-    opacity: 1;
-    border-radius: 0.4rem;
-    color: #fff;
-    border: 1px solid var(--color-carolina-blue-1);
-    background-color: var(--color-carolina-blue-1);
-  }
-
-  .table-filter-field,
-  .table-filter-operator {
-    position: relative;
-  }
-
-  .table-filter-field-select,
-  .table-filter-operator-select {
-    appearance: none;
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    background-color: var(--color-almost-black);
-    color: var(--color-text);
-    border: 1px solid var(--color-dark-grey-2);
-    padding: 0.3rem 1.5rem 0.4rem 0.45rem;
-    border-radius: 0.4rem;
-  }
-
-  .table-filter-field-icon,
-  .table-filter-operator-icon {
-    position: absolute;
-    top: 0;
-    right: 0;
-    padding: 0.4rem 0.3rem;
-    pointer-events: none;
-    background-color: transparent;
-  }
-
-  .table-filter-value {
-    background-color: var(--color-almost-black);
-    color: var(--color-text);
-    border: 1px solid var(--color-dark-grey-2);
-    border-radius: 0.4rem;
-    transition: 0.2s ease;
-    padding: 0.3rem 0.4rem;
-    outline: none;
-    flex: 1;
-  }
-
-  .table-filter-value:focus {
-    border: 1px solid var(--color-carolina-blue);
-  }
-
-  .table-filter-item-close,
-  .table-filter-button-add {
-    display: flex;
-    align-items: center;
-    padding: 0.3rem;
-    border-radius: 0.3rem;
-    background-color: var(--color-dark-grey-2);
-    border: 1px solid var(--color-dark-grey-2);
-    cursor: pointer;
-    transition: 0.2s ease;
-  }
-
-  .table-filter-item-close:hover,
-  .table-filter-button-add:hover {
-    background-color: var(--color-dark-grey);
-  }
-
-  .table-filter-buttons {
-    display: flex;
-    justify-content: flex-end;
-    padding: 0 1rem;
-    margin-top: 1rem;
-    gap: 0.75rem;
-    align-items: center;
-  }
-
-  .table-filter-button-submit {
-    padding: 0.4rem 0.6rem;
-    cursor: pointer;
-    color: #fff;
-    background-color: var(--color-carolina-blue-1);
-    border-radius: 0.3rem;
-    transition: 0.2s ease;
-  }
-
-  .table-filter-button-submit:hover {
-    background-color: var(--color-carolina-blue-2);
-  }
-
-  .table-data {
-    width: max-content;
-    border-spacing: 0;
-    padding: 0.75rem;
-  }
-
-  .table-data-head {
-    background-color: var(--color-almost-black);
-    border: 2px solid var(--color-dark-grey);
-  }
-
-  .table-data-body {
-    background-color: var(--color-dark-grey);
-  }
-
-  .table-data-cell {
-    padding: 0.2rem 0.4rem;
-    text-wrap: nowrap;
-  }
-
-  .table-data-head .table-data-cell {
-    font-weight: 600;
-    border-top: 2px solid var(--color-dark-grey);
-    border-bottom: 2px solid var(--color-dark-grey);
-    border-left: 2px solid var(--color-dark-grey);
-    cursor: pointer;
-    padding-right: 2rem;
-    position: relative;
-  }
-
-  .table-data-cell-sort {
-    position: absolute;
-    top: 0.2rem;
-    right: 0.4rem;
-    visibility: hidden;
-  }
-
-  .table-data-cell-sort.active {
-    visibility: visible;
-  }
-
-  .table-data-cell-icon {
-    display: flex;
-    align-items: center;
-  }
-
-  .table-data-body .table-data-cell {
-    border-bottom: 2px solid var(--color-almost-black);
-    border-left: 2px solid var(--color-almost-black);
-  }
-
-  .table-data-cell-null {
-    color: var(--color-medium-gray);
-  }
-
-  .table-data-cell-blue {
-    color: var(--color-carolina-blue);
-  }
-
-  .table-data-cell-green {
-    color: var(--color-dark-sage);
-  }
-
-  .table-data-cell-orange {
-    color: var(--color-dark-peach);
-  }
-
-  .table-data-cell-yellow {
-    color: var(--color-light-mustard);
   }
 </style>
---
+++ frontend/src/components/tables/TableRecord.svelte
@@ -0,0 +1,181 @@
+<script>
+  import { ArrowDownNarrowWide, ArrowUpWideNarrow } from "@lucide/svelte";
+
+  let {
+    queryTableContents,
+    updateQueryTableContents,
+    tableColumns,
+    tableRecords,
+  } = $props();
+
+  function getColumnTypeColorClass(type, data) {
+    if (data === null) {
+      return "table-record-cell-null";
+    }
+
+    switch (type) {
+      case "integer":
+      case "numeric":
+      case "double precision":
+        return "table-record-cell-blue";
+      case "character varying":
+      case "text":
+        return "table-record-cell-green";
+      case "timestamp with time zone":
+      case "timestamp without time zone":
+        return "table-record-cell-orange";
+      case "boolean":
+        return "table-record-cell-yellow";
+    }
+  }
+
+  function toggleColumnSort(tableColumn) {
+    // If sort_by changed, set order_by back to ASC
+    // If sort_by unchanged, toggle order_by
+    let sortBy = tableColumn.column_name;
+    let orderBy = "";
+    if (tableColumn.column_name != queryTableContents.sort_by) {
+      orderBy = "ASC";
+    } else {
+      if (queryTableContents.order_by == "ASC") {
+        orderBy = "DESC";
+      } else {
+        sortBy = "";
+        orderBy = "";
+      }
+    }
+    updateQueryTableContents({
+      table_name: queryTableContents.table_name,
+      sort_by: sortBy,
+      order_by: orderBy,
+      offset: queryTableContents.offset,
+    });
+  }
+</script>
+
+<table class="table-record">
+  <thead class="table-record-head">
+    <tr>
+      {#each tableColumns as tableColumn}
+        <th
+          class="table-record-cell"
+          onclick={(e) => {
+            e.preventDefault();
+            toggleColumnSort(tableColumn);
+          }}
+        >
+          {tableColumn.column_name}
+          <div
+            class="table-record-cell-sort {tableColumn.column_name ==
+            queryTableContents.sort_by
+              ? 'active'
+              : ''}"
+          >
+            {#if queryTableContents.order_by == "ASC"}
+              <div class="table-record-cell-icon">
+                <ArrowDownNarrowWide size="16" />
+              </div>
+            {:else}
+              <div class="table-record-cell-icon">
+                <ArrowUpWideNarrow size="16" />
+              </div>
+            {/if}
+          </div>
+        </th>
+      {/each}
+    </tr>
+  </thead>
+  <tbody class="table-record-body">
+    {#if tableRecords.length > 0}
+      {#each tableRecords as tableRecord}
+        <tr>
+          {#each tableColumns as tableColumn}
+            <td
+              class="table-record-cell {getColumnTypeColorClass(
+                tableColumn.data_type,
+                tableRecord[tableColumn.column_name],
+              )}"
+              >{tableRecord[tableColumn.column_name] === null
+                ? "NULL"
+                : tableRecord[tableColumn.column_name]}</td
+            >
+          {/each}
+        </tr>
+      {/each}
+    {/if}
+  </tbody>
+</table>
+
+<style>
+  /* Table Record */
+  .table-record {
+    width: max-content;
+    border-spacing: 0;
+    padding: 0.75rem;
+  }
+
+  .table-record-head {
+    background-color: var(--color-almost-black);
+    border: 2px solid var(--color-dark-grey);
+  }
+
+  .table-record-body {
+    background-color: var(--color-dark-grey);
+  }
+
+  .table-record-cell {
+    padding: 0.2rem 0.4rem;
+    text-wrap: nowrap;
+  }
+
+  .table-record-head .table-record-cell {
+    font-weight: 600;
+    border-top: 2px solid var(--color-dark-grey);
+    border-bottom: 2px solid var(--color-dark-grey);
+    border-left: 2px solid var(--color-dark-grey);
+    cursor: pointer;
+    padding-right: 2rem;
+    position: relative;
+  }
+
+  .table-record-cell-sort {
+    position: absolute;
+    top: 0.2rem;
+    right: 0.4rem;
+    visibility: hidden;
+  }
+
+  .table-record-cell-sort.active {
+    visibility: visible;
+  }
+
+  .table-record-cell-icon {
+    display: flex;
+    align-items: center;
+  }
+
+  .table-record-body .table-record-cell {
+    border-bottom: 2px solid var(--color-almost-black);
+    border-left: 2px solid var(--color-almost-black);
+  }
+
+  .table-record-cell-null {
+    color: var(--color-medium-gray);
+  }
+
+  .table-record-cell-blue {
+    color: var(--color-carolina-blue);
+  }
+
+  .table-record-cell-green {
+    color: var(--color-dark-sage);
+  }
+
+  .table-record-cell-orange {
+    color: var(--color-dark-peach);
+  }
+
+  .table-record-cell-yellow {
+    color: var(--color-light-mustard);
+  }
+</style>
